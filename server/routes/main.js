4c8764d3f50f9bb1a82005f5f6927ad155b5be1d
Syarif-Ys <5yarif.y.s@gmail.com>
2023-09-13 20:05:36 +0800

    mongodb

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
+/** 
+ * GET /
+ * HOME
+*/
 
-// Routes
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
 const locals = {
     title: "NodeJs Blog",
     description: "Simple Blog create with NodeJs, Express & MongoDb."
 }
 
-    res.render('index', { locals });
+try {
+    const data = await Post.find();
+    res.render('index', { locals, data });
+} catch (error) {
+    console.log(error);
+}
+
 });
+
+function insertPostData () {
+    Post.insertMany([
+        {
+            title: "Building a Blog",
+            body: "This is the body text"
+        },
+    ])
+}
+// insertPostData();
+
+
+
+
+
+
+
+
+
+
+
 
 router.get('/about', (req, res) => {
     res.render('about');
