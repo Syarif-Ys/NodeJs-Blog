a6fb428914e899b0d4f7e950168eb71843ed53ab
Syarif-Ys <5yarif.y.s@gmail.com>
2023-09-19 10:08:53 +0800

    router and cookie

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,7 +6,32 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
-const adminLayout = '../views/layouts/admin'
+const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+/** 
+ * GET /
+ * Check - Login
+*/
+
+const authMiddleware = (req, res, next ) => {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json ( { message: 'Unauthorized'});
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(401).json ( { message: 'Unauthorized'});
+    }
+}
+
+
+
 /** 
  * GET /
  * Admin - Login Page
@@ -34,17 +59,36 @@ router.post('/admin', async (req, res) => {
     try {
        const { username, password } = req.body;
        
-       if(req.body.username === 'admin' && req.body.password === 'password') {
-        res.send('You are logged in.');
-       } else {
-        res.send('Wrong username or password');
+       const user = await User.findOne( { username } );
+
+       if(!user) {
+        return res.status(401).json( { message: 'Invalid creadential' });
        }
+
+       const isPasswordValid = await bcrypt.compare(password, user.password);
+
+       if(!isPasswordValid) {
+        return res.status(401).json ( { message: 'Invalid credential' });
+       }
+
+       const token = jwt.sign( { userId:user._id }, jwtSecret );
+       res.cookie('token', token, {httpOnly: true} );
+
+       res.redirect('/dashboard');
 
     } catch (error) {
         console.log(error);
     }
-    
-    });
+});
+
+/** 
+ * 
+ * Redirect - Dashboard
+*/
+
+router.get('/dashboard', authMiddleware, async (req, res ) => {
+    res.render('admin/dashboard');
+});
 
     /** 
  * POST /
